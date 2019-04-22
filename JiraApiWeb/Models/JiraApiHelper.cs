33ffadf098aa4501e4b76e8de62e1cae1720979d
FrankXu <frankxu1992@gmail.com>
2019-04-22 09:29:41 -0700

    added pull all issues method and all users method

--- JiraApiWeb/Models/JiraApiHelper.cs
+++ JiraApiWeb/Models/JiraApiHelper.cs
@@ -1,11 +1,61 @@
-﻿using System;
+﻿using Atlassian.Jira;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace JiraApiWeb.Models
 {
-	public class JiraApiHelper
+	class JiraApiHelper
 	{
+		private readonly Jira inner_client;
+		public JiraApiHelper()
+		{
+			this.inner_client = Jira.CreateRestClient("http://bryczproject.atlassian.net", "frankxu@arctrade.com", "xxy921210");
+		}
+
+		public async Task<Issue> GetIssuseAsync()
+		{
+			var issue = await inner_client.RestClient.ExecuteRequestAsync<Issue>(Method.GET, $"/rest/api/3/issue/USG-979");
+
+			return issue;
+		}
+
+		public List<Issue> GetAllIssusesAsync()
+		{
+			var issues = new List<Issue>();
+			var search_result = inner_client.RestClient.ExecuteRequestAsync<Issue>(Method.GET, $"/rest/api/3/search?");
+			foreach (var issue in search_result)
+			{
+				issues.Add(issue);
+			}
+			return issues;
+		}
+
+		static string PrepareJqlbyDates(string beginDate, string endDate)
+		{
+			string jqlString = "project = PRJ AND status = Closed AND resolved >= " + beginDate + " AND resolved <= " + endDate;
+
+			return jqlString;
+		}
+
+		public List<User> GetAllUsers()
+
+		{
+			var users = new List<User>();
+
+			var search_result = inner_client.RestClient.ExecuteRequestAsync<List<User>>(Method.GET,
+				$"/rest/api/3/user/search?username=_&startAt=0&maxResults=2000");
+
+			foreach (var user in search_result)
+			{
+				if (!users.Any(x => x.Key.Equals(user.Key, StringComparison.CurrentCultureIgnoreCase)))
+					users.Add(user);
+			}
+
+			return users;
+		}
 	}
 }
---
+++ JiraApiWeb/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JiraApiWeb.Models
+{
+	public class User
+	{
+		public bool Active { get; set; }
+		public string DisplayName { get; set; }
+		public string EmailAddress { get; set; }
+		public string Key { get; set; }
+		public string Locale { get; set; }
+		public string Name { get; set; }
+		public string Self { get; set; }
+		public string TimeZone { get; set; }
+	}
+}
