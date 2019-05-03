b8d9c6dd919e0d293c3d5f764b96e25fc0e5687c
FrankXu <frankxu1992@gmail.com>
2019-05-03 10:39:54 -0700

    added two classes, calculation methods and database

---
+++ JiraApiWeb/Models/JiraChangelog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JiraApiWeb.Models
+{
+	public class JiraChangelog
+	{
+		public int startAt { get; set; }
+		public int maxResults { get; set; }
+		public int total { get; set; }
+		public List<Atlassian.Jira.IssueChangeLog> histories { get; set; }
+	}
+}
---
+++ JiraApiWeb/Models/JiraWorklog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Atlassian.Jira;
+
+namespace JiraApiWeb.Models
+{
+	public class JiraWorklog
+	{
+		public int startAt { get; set; }
+		public int maxResults { get; set; }
+		public int total { get; set; }
+		public List<Atlassian.Jira.Worklog> worklogs { get; set; }
+	}
+}
